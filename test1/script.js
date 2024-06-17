64d36aeb28ef20fcec9eb8edb489c3f3be23fc0a
Superhat Systems <154560006+superhatsystems@users.noreply.github.com>
2024-06-17 16:40:57 +0100

    Delete test1/script.js

--- test1/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const navMenu = document.querySelector('nav ul');
-    const menuButton = document.querySelector('.menu-button');
-
-    if (navMenu) {
-        const isVisible = navMenu.classList.toggle('visible');
-        menuButton.setAttribute('aria-expanded', isVisible);
-    }
-}
